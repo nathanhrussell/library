950fd4a4dbb74d34e64bc81dfbe86a2a6bc909df
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-01-30 17:44:38 +0100

    Finalise code with prototype method for adding books and looping through array

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+
+    Book.prototype.getDetails = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages`;
+    }
 }
 
 function addBookToLibrary(title, author, pages) {
-     new Book(title, author, pages)
+     myLibrary.push(new Book(title, author, pages));
 }
+
+addBookToLibrary("1984", "George Orwell", "328");
+myLibrary.forEach(book => console.log(book.getDetails()));
+
+
